bf50535553425186f3f6f4cce0ac7a8f25db47a5
Ben <bullich.benjamin@gmail.com>
2015-11-02 14:57:14 +0100

    ok

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer>
+	<div class="fin">
+		<div class="fin1">
+	<a href="http://simplon.co/"><p><B>Site simplon.co</B></p></a>
+  	<img src="http://ulule.me/presales/5/6/6/10665/logo_simplonco_rect_coul_blanc400px_png_640x860_q85.jpg" alt="Nouveau!" />
+	</div>
+		<div class="fin2">
+	<a href="http://entreprendre.legrandnarbonne.com/717-iness.html"><p align="right"><B>Site INESS</B></p></a>
+	</div>
+		<div class="fin3">
+	<img src="http://entreprendre.legrandnarbonne.com/uploads/pics/img_INESS.jpg" alt="Nouveau!" />
+</div>
+<div class="rageux">
+	<p><h6 align="center">realise by Benjamin Bullich , Thomas Ngoma , Edouard Gravez	  						</h6></p>
+
+</div>
+</div>
+</footer>
