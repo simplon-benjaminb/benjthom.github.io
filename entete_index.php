cb4e6b562a891a84ec4007a29689f709c7a09201
Thomas <thomas.ngoma@live.fr>
2015-11-02 15:33:18 +0100

    Ajout entete thomas

---
+++ entete_index.php
@@ -0,0 +1,16 @@
+<header>
+
+	<a href="http://simplonline.co/"><img src="http://img11.hostingpics.net/pics/576295simplon.png" alt ="Logo simplon" id="logo" /></a>
+	
+	<div id="auteurs">
+	<h2>Benjamin - Edouard - Thomas</h2>
+	</div>
+	
+	<div id="titre">
+	<h1>TROMBI COMMUNAUTE SIMPLON NARBONNE</h1> 
+	</div>
+		
+	
+</header>
+
+
