bc3ad53b47a643b4a1e3c1b69c2b509c5f7d3501
Ben <bullich.benjamin@gmail.com>
2015-11-02 16:31:29 +0100

    c

---
+++ promo.php
@@ -0,0 +1,13 @@
+<article>
+  <div class="article1">
+    <h2><B>PROMO SIMPLON.co 2015-2016 Narbonne</B></h2>
+  </div>
+<div class="article2">
+  <h4><B>Qu'ils sont beau...</B></h3>
+  </div>
+
+  <div class="article3">
+<img src="https://pbs.twimg.com/profile_images/378800000016910707/80f09b042a8c2a2b53f06b391ab63ab4_400x400.png" />
+</div>
+
+</article>
